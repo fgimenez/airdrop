9ce526d870b04ce91192eab1fc2f10fbc6528c01
Federico Gimenez <federico@giantswarm.io>
2018-05-12 21:46:22 +0200

    drop happy path

--- contracts/Airdrop.sol
+++ contracts/Airdrop.sol
@@ -1,18 +1,31 @@
 pragma solidity ^0.4.23;
 
+import "zeppelin-solidity/contracts/access/SignatureBouncer.sol";
 import "zeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
 /**
  * @title Airdrop
  * @dev Basic Airdrop contract.
  */
-contract Airdrop {
+contract Airdrop is SignatureBouncer {
   // The token being dropped.
   ERC20 public token;
 
-  constructor(ERC20 _token) public {
+  // The amount of tokens to be dropped on each request.
+  uint256 public dropAmount;
+
+  constructor(ERC20 _token, uint256 _dropAmount) public {
+    require(_dropAmount > 0);
     require(_token != address(0));
 
     token = _token;
+    dropAmount = _dropAmount;
+  }
+
+  /**
+   * @dev request tokens with signature.
+   */
+  function requestTokens(bytes _sig) public onlyValidSignature(_sig) {
+    token.transfer(msg.sender, dropAmount);
   }
 }
--- test/Airdrop.test.js
+++ test/Airdrop.test.js
@@ -1,4 +1,6 @@
+
 import assertRevert from './helpers/assertRevert';
+import { signHex } from './helpers/sign';
 
 const BigNumber = web3.BigNumber;
 
@@ -7,28 +9,49 @@ const should = require('chai')
   .use(require('chai-bignumber')(BigNumber))
   .should();
 
+export const getSigner = (contract, signer, data = '') => (addr) => {
+  // via: https://github.com/OpenZeppelin/zeppelin-solidity/pull/812/files
+  const message = contract.address.substr(2) + addr.substr(2) + data;
+  // ^ substr to remove `0x` because in solidity the address is a set of byes, not a string `0xabcd`
+  return signHex(signer, message);
+};
+
 const Airdrop = artifacts.require('Airdrop');
 const SimpleToken = artifacts.require('SimpleToken');
 
-contract('Airdrop', () => {
+contract('Airdrop', ([_, owner, authorizedUser, bouncerAddress]) => {
   const tokenSupply = new BigNumber('1e22');
+  const dropAmount = 1000;
 
-  beforeEach(async function() {
+  before(async function() {
     this.token = await SimpleToken.new();
-    this.airdrop = await Airdrop.new(this.token.address);
+    this.airdrop = await Airdrop.new(this.token.address, dropAmount, { from: owner });
+
     await this.token.transfer(this.airdrop.address, tokenSupply);
+
+    this.genSig = getSigner(this.airdrop, bouncerAddress);
+    await this.airdrop.addBouncer(bouncerAddress, { from: owner });
   });
 
   describe('when token address is 0x0', () => {
     it('creation reverts', async function() {
       const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-      await assertRevert(Airdrop.new(ZERO_ADDRESS));
+      await assertRevert(Airdrop.new(ZERO_ADDRESS, dropAmount));
     });
   });
 
-  describe('basic withdrawal', () => {
-    it('should assign tokens to allowed bouncer', async function() {
+  describe('when drop amount 0', () => {
+    it('creation reverts', async function() {
+      await assertRevert(Airdrop.new(this.token.address, 0));
+    });
+  });
 
+  describe('basic token requests', () => {
+    it('should assign tokens to allowed bouncer', async function() {
+      const validSignature = this.genSig(authorizedUser);
+      await this.airdrop.requestTokens(validSignature, { from: authorizedUser});
+      const balance = await this.token.balanceOf(authorizedUser);
+      assert.equal(balance, dropAmount);
     });
 
     it('should revert not allowed bouncer', async function() {
